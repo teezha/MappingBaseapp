0cac4fea7e63a68d0f4f76a135353cf6b1e25b90
teezha <genggengz@gmail.com>
2017-02-24 17:25:32 -0800

    second commit completed most of base code require to fix zoom button next steps remove the zoom button

--- mobile/src/main/java/exp/bcit/teezha/mappingbaseapp/MainActivity.java
+++ mobile/src/main/java/exp/bcit/teezha/mappingbaseapp/MainActivity.java
@@ -1,20 +1,45 @@
 package exp.bcit.teezha.mappingbaseapp;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.esri.android.map.MapView;
 import com.esri.android.map.ags.ArcGISTiledMapServiceLayer;
 import com.esri.android.map.event.OnStatusChangedListener;
+import com.esri.core.geometry.Envelope;
+import com.esri.core.geometry.Point;
+import com.esri.core.map.Feature;
+import com.esri.core.map.FeatureResult;
+import com.esri.core.tasks.query.QueryParameters;
+import com.esri.core.tasks.query.QueryTask;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity implements OnStatusChangedListener {
 
     MapView mapView;
+    Button mBtnZoom;
+    Spinner mSpnParcels;
+
+    int mLotLayerID = 0;
+    String mLotLayerURL;
+    String mLotNumColName = "PARCELS_ID";
+
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -27,8 +52,57 @@ public class MainActivity extends AppCompatActivity implements OnStatusChangedLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+
         addBCLayer();
         getMapView().setOnStatusChangedListener(this);
+
+        this.mBtnZoom = (Button) findViewById(R.id.btnZoom);
+        this.mSpnParcels = (Spinner) findViewById(R.id.spnParcels);
+
+        QueryParameters qryLotNums = new QueryParameters();
+        qryLotNums.setReturnGeometry(false);
+        qryLotNums.setOutFields(new String[]{mLotNumColName});
+        qryLotNums.setWhere(mLotNumColName + " > 0");
+
+        mLotLayerURL = getString(R.string.basemap_url) + "/" + mLotLayerID;
+        QueryTask qtask = new QueryTask(mLotLayerURL);
+
+
+        try {
+            /** ================================
+             * add your code here
+             * ==================================*/
+            FeatureResult fSet = qtask.execute(qryLotNums);
+
+            ArrayList<Integer> listOfLotNums = new ArrayList<Integer>();
+            Feature tmpFeat;
+
+            for (Object featAsObj : fSet) {
+                tmpFeat = (Feature) featAsObj;
+                listOfLotNums.add(
+                        Integer.valueOf(tmpFeat.getAttributeValue(
+                                mLotNumColName).toString()));
+            }
+            Collections.sort(listOfLotNums);
+
+            ArrayAdapter<Integer> adtTmp = new ArrayAdapter<Integer>(
+                    this, android.R.layout.simple_spinner_dropdown_item,
+                    listOfLotNums);
+
+            mSpnParcels.setAdapter(adtTmp);
+
+
+        } catch (Exception e) {
+            Log.d("GIST-8010", e.getMessage());
+        }
+//==========================================
+// import com.esri.core.tasks.query
+//==========================================
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -76,10 +150,70 @@ public class MainActivity extends AppCompatActivity implements OnStatusChangedLi
         ArcGISTiledMapServiceLayer basemap = new ArcGISTiledMapServiceLayer(
                 this.getResources().getString(R.string.basemap_url));
         getMapView().addLayer(basemap);
+    }
+
+
+    /**
+     * =============================================================================
+     * Zooms the map the the MBR of the feature selected in the spinner
+     * import android.view.View;
+     * =============================================================================
+     */
+    public void zoomToFeature(View v) {
+
+        /** =============================================================================
+         * Your code goes here
+         * ============================================================================= */
+
+        quickToast("Inside ZoomToFeature");
+
+        QueryParameters qryLotNums = new QueryParameters();
+        qryLotNums.setReturnGeometry(true);
+        qryLotNums.setOutFields(new String[]{mLotNumColName});
+        qryLotNums.setWhere((String) mSpnParcels.getSelectedItem());
+
+        mLotLayerURL = getString(R.string.basemap_url) + "/" + mLotLayerID;
+        QueryTask qtask = new QueryTask(mLotLayerURL);
+
+        try {
+            /** ================================
+             * add your code here
+             * ==================================*/
+            FeatureResult fSet = qtask.execute(qryLotNums);
+            Feature tmpFeat = (Feature) fSet.iterator().next();
+
+            Envelope epFeat = new Envelope((Point) tmpFeat.getGeometry());
+
+            getMapView().setExtent(epFeat);
+
+
+
+
+
+
+
+
+        } catch (Exception e) {
+            Log.d("GIST-8010", e.getMessage());
+        }
+
 
 
     }
 
+
+    /**
+     * =============================================================================
+     * This method simplifies making a toast
+     * =============================================================================
+     */
+    public void quickToast(String message) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
+    }
 
 
     /**
@@ -120,8 +254,8 @@ public class MainActivity extends AppCompatActivity implements OnStatusChangedLi
 
     @Override
     public void onStatusChanged(Object o, STATUS status) {
-        if(status == STATUS.LAYER_LOADED) {
-            zoom();
+        if (status == STATUS.LAYER_LOADED) {
+
 
         }
 
@@ -129,19 +263,4 @@ public class MainActivity extends AppCompatActivity implements OnStatusChangedLi
     }
 
 
-    public void zoom() {
-        /** =============================================================================
-         * zoom the map to a known location: crescent beach
-         * =============================================================================*/
-        double lat, lon;
-        lat = 49.060369722746124;
-        lon = -122.87796020507814;
-        boolean animated = true;
-        double scale = 40000;
-        /** =============================================================================
-         * 2 step zoom: center then scale
-         * =============================================================================*/
-        getMapView().centerAt(lat, lon, animated);
-        getMapView().setScale(scale);
-    }
 }
